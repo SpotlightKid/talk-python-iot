bf2b5f9288e6fd0f83145ea6325823a27ae71b2b
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2016-08-24 14:05:31 +0200

    Make probe function global

--- src/esp8266/probepins.py
+++ src/esp8266/probepins.py
@@ -1,16 +1,17 @@
+# probe_pins.py
+
 from machine import Pin
-from time import  sleep_ms
+from time import sleep_ms
 
 
-def main():
-    def probe(ms):
-        for i in (0,2,5, 12, 13, 14, 15, 16):
-            print("Probing pin %i" % i)
-            pin = Pin(i, Pin.OUT)
-            pin.low()
-            sleep_ms(ms)
-            pin.high()
-            del pin
+def probe(ms):
+    for i in (0, 2, 5, 12, 13, 14, 15, 16):
+        print("Probing pin %i" % i)
+        pin = Pin(i, Pin.OUT)
+        pin.low()
+        sleep_ms(ms)
+        pin.high()
+        del pin
 
 
 #probe(2000)
